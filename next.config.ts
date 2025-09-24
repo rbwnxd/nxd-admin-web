bb519ff79530126d40ddcb7cae44feb6d9dc1af4
woosung <monnalija@naver.com>
2025-09-24 19:02:35 +0900

    Etc: next image 도메인 화이트리스트 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "nxd-storage-test.s3.ap-northeast-2.amazonaws.com",
+      "storage.nxd-c.com",
+    ],
+    formats: ["image/avif", "image/webp"],
+  },
 };
 
 export default nextConfig;
